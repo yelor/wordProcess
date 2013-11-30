c2f6eb0b1f143323be6f2c41f13d9ba0ff8fd45a
huang yurong <307523779@qq.com>
2013-11-30 19:21:43 +0800

    上传文件
    wordprocess的所有代码

---
+++ WordProcess/WordHistory/build/classes-generated/org/word/editor/history/Bundle.java
@@ -0,0 +1,27 @@
+package org.word.editor.history;
+/** Localizable strings for {@link org.word.editor.history}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>WordHistory</i>
+     * @see WordHistoryTopComponent
+     */
+    static String CTL_WordHistoryAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_WordHistoryAction");
+    }
+    /**
+     * @return <i>WordHistory Window</i>
+     * @see WordHistoryTopComponent
+     */
+    static String CTL_WordHistoryTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_WordHistoryTopComponent");
+    }
+    /**
+     * @return <i>This is a WordHistory window</i>
+     * @see WordHistoryTopComponent
+     */
+    static String HINT_WordHistoryTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_WordHistoryTopComponent");
+    }
+    private void Bundle() {}
+}
